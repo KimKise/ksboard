f3f5fa8af21d2a03cc46b6ed66ccc0c32ec48e83
kise <kise@192.168.122.5>
2017-02-04 11:56:23 +0900

    프로젝트 등록

---
+++ z-board/WebContent/board/boardList.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<script src="http://code.jquery.com/jquery-3.1.1.js"></script>
+<title>Insert title here</title>
+<%@ include file ="../include/header.jsp" %>
+<script>
+
+</script>
+</head>
+<body>
+<table border ="1" width="800px">
+	<tr>
+		<th>번호</th>
+		<th>이름</th>
+		<th>제목</th>
+		<th>날짜</th>
+		<th>조회수</th>
+		<th>첨부파일</th>
+		<th>다운로드</th>
+	</tr>
+	
+</table>
+</body>
+</html>
---
+++ z-board/src/config/MybatisService.java
@@ -0,0 +1,29 @@
+﻿package config;
+
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MybatisService {
+	//SqlSessionFactoryBuilder	=>	SqlSessionFactory	=>	SqlSession을 만들어냄
+	//SqlSession 객체 생성기
+	private static SqlSessionFactory factory;
+	static {
+		try { 
+			//Resources는 java Resources의 src를 검색
+			//sqlMapConfig.xml myBatis설정 파일을 읽어 들임
+			Reader r = Resources.getResourceAsReader("config/sqlMapConfig.xml");
+			//SqlSettionFactory 생성기
+			factory = new SqlSessionFactoryBuilder().build(r);
+			r.close();	//Reader를 닫음.
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static SqlSessionFactory getFactory() {
+		return factory;
+	}
+}
